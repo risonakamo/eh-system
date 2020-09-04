b02b1bb9bd89d11bfd649b83b29df22e7b334e95
risonakamo <hmykanae@gmail.com>
2020-09-04 01:20:21 -0400

    album count label

--- albumexplore/components/album-tile/albumtile.tsx
+++ albumexplore/components/album-tile/albumtile.tsx
@@ -7,7 +7,8 @@ export default class AlbumTile extends React.Component
   render()
   {
     return <div className="album-tile">
-      <div className="title">randomboobguy</div>
+      <div className="title float-label">randomboobguy</div>
+      <div className="item-count float-label">23</div>
       <img src="/thumbnaildata/deadflow/neflim/05.jpg"/>
     </div>;
   }
