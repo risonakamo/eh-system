eb6e9964d90a546f0c5221c86e9a1d1247789eae
risonakamo <hmykanae@gmail.com>
2020-09-04 22:33:00 -0400

    more album tile styling date field

--- albumexplore/components/album-tile/albumtile.tsx
+++ albumexplore/components/album-tile/albumtile.tsx
@@ -9,6 +9,7 @@ export default class AlbumTile extends React.Component
     return <div className="album-tile">
       <div className="title float-label">randomboobguy</div>
       <div className="item-count float-label">23</div>
+      <div className="date float-label">20/02/01</div>
       <img src="/thumbnaildata/deadflow/neflim/05.jpg"/>
     </div>;
   }
