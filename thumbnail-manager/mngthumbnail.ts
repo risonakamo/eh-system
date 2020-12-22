ffce91ad8c8515b3f4c23175d0f4aa78079f3383
risonakamo <hmykanae@gmail.com>
2020-12-22 00:39:50 -0500

    generate image thumbnails

--- thumbnail-manager/mngthumbnail.ts
+++ thumbnail-manager/mngthumbnail.ts
@@ -3,17 +3,21 @@ import normalise from "normalize-path";
 import _ from "lodash";
 import replaceExt from "replace-ext";
 import {join,dirname,extname} from "path";
+import imageThumbnail from "image-thumbnail";
+import {ensureDirSync,writeFile} from "fs-extra";
 
-const _imageDataDir:string="../../h/cg";
-const _thumbnailDataDir:string="../thumbnaildata2";
+// PATHS SHOULD BE RELATIVE TO THE CURRENT DIRECTORY EXECUTING THE FILE FROM, NOT WHERE THIS FILE IS LOCATED
+const _imageDataDir:string="../../h/cg/grimgrim";
+const _thumbnailDataDir:string="thumbnaildata2";
 const _batchSize:number=5;
 
 async function main():Promise<void>
 {
-    var paths:string[]=await getDirItems(njoin(_imageDataDir,"grimgrim"));
+    var paths:string[]=await getDirItems(_imageDataDir);
     var jobs:ThumbnailGenJob[]=resolveThumbnailJobs(_imageDataDir,_thumbnailDataDir,paths);
 
-    console.log(jobs);
+    console.log(jobs[0]);
+    generateImageThumbnail(jobs[0].fullPath,jobs[0].thumbnailPath);
 }
 
 /** return path of files, relative to the intially given target path */
@@ -27,7 +31,8 @@ async function getDirItems(target:string):Promise<string[]>
     });
 }
 
-/** resolve array of paths RELATIVE TO THE BASEPATH to extra information including thumbnail output location*/
+/** resolve array of paths RELATIVE TO THE BASEPATH to extra information including thumbnail output location.
+ *  basepath and thumbnail dir should be relative to the cwd.*/
 function resolveThumbnailJobs(basepath:string,thumbnaildir:string,relpaths:string[]):ThumbnailGenJob[]
 {
     return _.map(relpaths,(x:string):ThumbnailGenJob=>{
@@ -41,6 +46,33 @@ function resolveThumbnailJobs(basepath:string,thumbnaildir:string,relpaths:strin
             thumbnailPath,
             originalExt:extname(x)
         };
+    });
+}
+
+/** async generate thumbnail for given full path to an image. give it the FULL PATH to the output,
+ *  including the file extension
+ *  - path: full path to the image from the cwd
+ *  - outputPath: path to the output image, as a jpg, from the cwd*/
+async function generateImageThumbnail(path:string,outputPath:string):Promise<void>
+{
+    var thumbnail:Buffer=await imageThumbnail(path,{
+        width:180,
+        height:180,
+        responseType:"buffer",
+        jpegOptions:{
+            force:true,
+            quality:80
+        },
+        fit:"cover"
+    } as any);
+
+    ensureDirSync(dirname(outputPath));
+
+    writeFile(outputPath,thumbnail,(err:NodeJS.ErrnoException)=>{
+        if (err)
+        {
+            console.log("image thumbnail generate write err",err);
+        }
     });
 }
 
