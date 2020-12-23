7e7ebd589049e9b952926dd16898f6a08e3f2c1e
risonakamo <hmykanae@gmail.com>
2020-12-23 00:10:28 -0500

    move generators into file

---
+++ thumbnail-manager/generators.ts
@@ -0,0 +1,46 @@
+import imageThumbnail from "image-thumbnail";
+import videoThumbnail from "video-thumbnail-generator";
+import {ensureDirSync,writeFile} from "fs-extra";
+import {basename,dirname} from "path";
+
+/** async generate thumbnail for given full path to an image. give it the FULL PATH to the output,
+ *  including the file extension
+ *  - path: full path to the image from the cwd
+ *  - outputPath: path to the output image, as a jpg, from the cwd*/
+export async function generateImageThumbnail(path:string,outputPath:string):Promise<void>
+{
+    var thumbnail:Buffer=await imageThumbnail(path,{
+        width:180,
+        height:180,
+        responseType:"buffer",
+        jpegOptions:{
+            force:true,
+            quality:80
+        },
+        fit:"cover"
+    } as any);
+
+    ensureDirSync(dirname(outputPath));
+
+    writeFile(outputPath,thumbnail,(err:NodeJS.ErrnoException)=>{
+        if (err)
+        {
+            console.log("image thumbnail generate write err",err);
+        }
+    });
+}
+
+/** async generate thumbnail for a full path to a video. give full path to the output, including extension.*/
+export async function generateVideoThumbnail(target:string,outputPath:string):Promise<void>
+{
+    ensureDirSync(dirname(outputPath));
+
+    await new videoThumbnail({
+        sourcePath:target,
+        thumbnailPath:dirname(outputPath)
+    }).generate({
+        size:"200x?",
+        count:1,
+        filename:basename(outputPath)
+    });
+}
--- thumbnail-manager/mngthumbnail.ts
+++ thumbnail-manager/mngthumbnail.ts
@@ -2,10 +2,9 @@ import recursiveDir from "recursive-readdir";
 import normalise from "normalize-path";
 import _ from "lodash";
 import replaceExt from "replace-ext";
-import {join,dirname,extname,basename} from "path";
-import imageThumbnail from "image-thumbnail";
-import videoThumbnail from "video-thumbnail-generator";
-import {ensureDirSync,writeFile} from "fs-extra";
+import {join,dirname,extname} from "path";
+
+import {generateVideoThumbnail,generateImageThumbnail} from "./generators";
 
 // PATHS SHOULD BE RELATIVE TO THE CURRENT DIRECTORY EXECUTING THE FILE FROM, NOT WHERE THIS FILE IS LOCATED
 const _imageDataDir:string="../../h/cg/grimgrim";
@@ -52,48 +51,6 @@ function resolveThumbnailJobs(basepath:string,thumbnaildir:string,relpaths:strin
             thumbnailPath,
             originalExt:extname(x)
         };
-    });
-}
-
-/** async generate thumbnail for given full path to an image. give it the FULL PATH to the output,
- *  including the file extension
- *  - path: full path to the image from the cwd
- *  - outputPath: path to the output image, as a jpg, from the cwd*/
-async function generateImageThumbnail(path:string,outputPath:string):Promise<void>
-{
-    var thumbnail:Buffer=await imageThumbnail(path,{
-        width:180,
-        height:180,
-        responseType:"buffer",
-        jpegOptions:{
-            force:true,
-            quality:80
-        },
-        fit:"cover"
-    } as any);
-
-    ensureDirSync(dirname(outputPath));
-
-    writeFile(outputPath,thumbnail,(err:NodeJS.ErrnoException)=>{
-        if (err)
-        {
-            console.log("image thumbnail generate write err",err);
-        }
-    });
-}
-
-/** async generate thumbnail for a full path to a video. give full path to the output, including extension.*/
-async function generateVideoThumbnail(target:string,outputPath:string):Promise<void>
-{
-    ensureDirSync(dirname(outputPath));
-
-    await new videoThumbnail({
-        sourcePath:target,
-        thumbnailPath:dirname(outputPath)
-    }).generate({
-        size:"200x?",
-        count:1,
-        filename:basename(outputPath)
     });
 }
 
