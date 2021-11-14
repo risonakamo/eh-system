b41e15888af87eada0569b31d1bcb82dd527ef8d
risonakamo <hmykanae@gmail.com>
2021-11-14 17:25:58 -0500

    resolving thumbnails to generate

--- server/lib/thumbnail-gen2/thumbnail-gen.ts
+++ server/lib/thumbnail-gen2/thumbnail-gen.ts
@@ -2,6 +2,7 @@ import recursiveDir from "recursive-readdir";
 import normalise from "normalize-path";
 import _ from "lodash";
 import {relative,resolve,join,parse} from "path";
+import {access} from "fs/promises";
 
 /** resolve information for all items recursively in target directory */
 async function getTargetFiles(inputdir:string):Promise<TargetItem[]>
@@ -20,19 +21,48 @@ async function getTargetFiles(inputdir:string):Promise<TargetItem[]>
 }
 
 /** convert target items to absolute thumbnail paths */
-function resolveThumbnailPaths(targets:TargetItem[],outputdir:string):string[]
+function resolveThumbnailPaths(targets:TargetItem[],outputdir:string):ThumbnailTargetItem[]
 {
     outputdir=normalise(resolve(outputdir));
 
-    return _.map(targets,(x:TargetItem):string=>{
-        return normalise(join(outputdir,x.filepath,x.filename+".jpg"));
+    return _.map(targets,(x:TargetItem):ThumbnailTargetItem=>{
+        return {
+            originalpath:x.fullpath,
+            thumbnailpath:normalise(join(outputdir,x.filepath,x.filename+".jpg"))
+        };
     });
+}
+
+/** filter array of thumbnail target items to only items where the target thumbnail does NOT exist */
+async function determineThumbnailsNeedGeneration(thumbnailtargets:ThumbnailTargetItem[])
+:Promise<ThumbnailTargetItem[]>
+{
+    // async check all items for existance. IF IT EXISTS, IT WILL BE NULL. so the only remaining items
+    // should all be items that DO NOT exist
+    const existCheck:Promise<ThumbnailTargetItem|null>[]=_.map(
+        thumbnailtargets,
+        async (x:ThumbnailTargetItem):Promise<ThumbnailTargetItem|null>=>{
+            try
+            {
+                await access(x.thumbnailpath);
+                return null;
+            }
+
+            catch
+            {
+                return x;
+            }
+        }
+    );
+
+    // wait for all checks to resolve. then filter out all nulls
+    return _.filter(await Promise.all(existCheck)) as ThumbnailTargetItem[];
 }
 
 function main()
 {
-    getTargetFiles("C:/Users/ktkm/Desktop/h/cg/healthyman").then((res:TargetItem[])=>{
-        console.log(resolveThumbnailPaths(res,"./thumbnaildata2"));
+    getTargetFiles("C:/Users/ktkm/Desktop/h/3d/nagoo").then(async (res:TargetItem[])=>{
+        console.log(await determineThumbnailsNeedGeneration(resolveThumbnailPaths(res,"./thumbnaildata/nagoo")));
     });
 }
 
