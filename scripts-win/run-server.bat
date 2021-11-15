df7480cfda2460245430a4b88d32f9cf1e43be84
risonakamo <hmykanae@gmail.com>
2021-11-14 21:05:19 -0500

    windows scripts

---
+++ scripts-win/run-server.bat
@@ -0,0 +1,5 @@
+@REM run the server using the current config
+
+cd %~dp0
+cd ..
+call npm run run-server
---
+++ scripts-win/run-thumbnail-gen.bat
@@ -0,0 +1,6 @@
+@REM run thumbnail generation
+
+cd %~dp0
+cd ..
+call npm run run-gen-thumbnails
+pause
