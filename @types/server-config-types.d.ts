99310c1b1c737a6445def169d7d1b4e96e87cd91
risonakamo <hmykanae@gmail.com>
2021-12-09 23:03:37 -0500

    starting on cloud mod

--- @types/server-config-types.d.ts
+++ @types/server-config-types.d.ts
@@ -3,6 +3,11 @@ interface ServerConfig
     imagedir:string
     thumbnaildir:string
 
+    google_cloud_config:{
+        enabled:boolean
+        mainimage_bucket:string
+    }
+
     port:number
     static_serve:boolean
     thumbnail_gen_batch:number
--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import serveIndex from "serve-index";
 import chalk from "chalk";
+import _ from "lodash";
+import {Bucket,Storage} from "@google-cloud/storage";
 
 import {getImagesInPath2Flat} from "./lib/imagedata-service";
 import {getAlbumInfo} from "./lib/album-service";
 import {getServerConfig} from "./lib/server-config";
+import {getCloudImageData} from "./lib/googlecloud/cloud-imagedata";
 
 function main()
 {
@@ -14,6 +17,16 @@ function main()
     const fullThumbnailDataDir:string=serverConfig.thumbnaildir;
 
     const app=express();
+
+    var mainCloudBucket:Bucket|undefined;
+    if (serverConfig.google_cloud_config.enabled)
+    {
+        const storage=new Storage({
+            keyFilename:`${__dirname}/../config/cloudkey.json`
+        });
+
+        mainCloudBucket=storage.bucket(serverConfig.google_cloud_config.mainimage_bucket);
+    }
 
     // --- static serving ---
     if (serverConfig.static_serve)
@@ -52,10 +65,18 @@ function main()
 
     // --- apis ---
     // get an album from the image data folder. also generate thumbnails if needed.
-    app.post("/get-album",express.text(),(req,res)=>{
+    app.post("/get-album",express.text(),async (req,res)=>{
         console.log("get album:",req.body);
 
-        res.json(getImagesInPath2Flat(fullImageDataDir,req.body));
+        if (!serverConfig.google_cloud_config.enabled)
+        {
+            res.json(getImagesInPath2Flat(fullImageDataDir,req.body));
+        }
+
+        else if (mainCloudBucket)
+        {
+            res.json(_.flatten(await getCloudImageData(req.body,mainCloudBucket)));
+        }
     });
 
     // given a target album path, retrieve album information for that path.
@@ -68,8 +89,17 @@ function main()
 
     app.listen(serverConfig.port,()=>{
         console.log(chalk.green("EH-SYSTEM started"));
-        console.log("image dir:",chalk.yellow(fullImageDataDir));
-        console.log("thumbnail dir:",chalk.yellow(fullThumbnailDataDir));
+
+        if (!serverConfig.google_cloud_config.enabled)
+        {
+            console.log("image dir:",chalk.yellow(fullImageDataDir));
+            console.log("thumbnail dir:",chalk.yellow(fullThumbnailDataDir));
+        }
+
+        else
+        {
+            console.log("cloud img dir:",chalk.yellow(serverConfig.google_cloud_config.mainimage_bucket));
+        }
     });
 }
 
