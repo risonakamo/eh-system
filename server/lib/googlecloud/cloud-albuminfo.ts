da917ddcf69bc364008d6376946047a3d888d9e8
risonakamo <hmykanae@gmail.com>
2021-12-11 11:55:08 -0500

    trying things

--- server/lib/googlecloud/cloud-albuminfo.ts
+++ server/lib/googlecloud/cloud-albuminfo.ts
@@ -1,6 +1,25 @@
+import _ from "lodash";
 import {Bucket,File} from "@google-cloud/storage";
 
-// function getCloudAlbumInfo(targetpath:string,bucket:Bucket):AlbumInfo[]
-// {
+import {getCloudImageDataDict,getImmediateDirNames,getImmediateDirs} from "./cloud-imagedata";
 
-// }
+async function getCloudAlbumInfo(targetpath:string,bucket:Bucket):AlbumInfo[]
+{
+    const imagedata:ImageDataDirs=await getCloudImageDataDict(targetpath,bucket);
+    const topDirs:string[]=getImmediateDirNames(targetpath,imagedata);
+
+    return _.map(topDirs,(topDirName:string):AlbumInfo=>{
+        const subtargetPath:string=`${targetpath}/${topDirName}`;
+        const subDirData:ImageDataDirs=getImmediateDirs(subtargetPath,imagedata);
+        const topOfSubDir:string[]=getImmediateDirNames(subtargetPath,subDirData);
+
+        return {
+            title:topDirName,
+            items:0,
+            immediateItems:topOfSubDir.length,
+            img:"",
+            date:"",
+            album:false
+        };
+    });
+}
